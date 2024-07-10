da027ef5c0aeebf9c0ee835a90b9defb1570b059
Ahmed Elaguab <elaguabahmed97@gmail.com>
2024-07-10 15:05:11 +0100

    add basic search page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main className="p-24">
-      <p>Next Weather App</p>
+      <h1 className="h1 text-2xl font-semibold">Next Weather App</h1>
+      <div className="flex flex-col mt-3">
+        <label htmlFor="search">Search for local weather</label>
+        <input
+          id="search"
+          className="border rounded p-2 mt-1"
+          type="text"
+          placeholder="Search cities.."
+        />
+      </div>
     </main>
   );
 }
